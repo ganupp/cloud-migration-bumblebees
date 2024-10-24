d6f003447ff7c6c31aeab3b4401344f3d9090c5a
SreePranavi <letters.to.pranavi@gmail.com>
2024-10-24 13:01:53 +0530

    moving things around

--- parserOcs/parser.py
+++ parserOcs/parser.py
@@ -21,22 +21,3 @@ class ParserOcs:
                                 self.parsed_vars[key] = value.get("default", None)
 
         return self.parsed_vars
-
-
-# def main():
-#     # Path to the folder containing Terraform files
-#     tf_folder_path = "terraform/infra"
-
-#     # Step 1: Parse all the Terraform files in the folder to get the variables
-#     self.parsed_vars = parse_tf_files_in_folder(tf_folder_path)
-
-#     # Print all parsed variables
-#     print("Parsed Terraform Variables:")
-#     for key, value in self.parsed_vars.items():
-#         print(f"{key}: {value}")
-
-#     # Step 2: Create Azure resources using the parsed variables
-#     # create_azure_resource(self.parsed_vars)
-
-# if __name__ == "__main__":
-#     main()
